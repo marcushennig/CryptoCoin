07dc432cc013dad59c709742982eb18cdc43a8c2
Marcus Hennig <marcus.hennig@d-fine.de>
2018-01-15 21:23:35 +0100

    Added output and input class

---
+++ src/main/java/coin/Input.java
@@ -0,0 +1,93 @@
+package coin;
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Input of an transaction
+ */
+public class Input {
+
+    /** Hash of the Transaction whose output is being used */
+    public byte[] prevTxHash;
+
+    /** Used output's index in the previous transaction */
+    public int outputIndex;
+
+    /** The signature produced to check validity */
+    public byte[] signature;
+
+    /**
+     * Constructor
+     * @param prevHash Hash of previous transaction
+     * @param index Index
+     */
+    public Input(byte[] prevHash, int index) {
+
+        if (prevHash == null) {
+
+            prevTxHash = null;
+
+        } else {
+
+            prevTxHash = Arrays.copyOf(prevHash, prevHash.length);
+
+        }
+        outputIndex = index;
+    }
+
+    /**
+     *
+     * @param sig
+     */
+    public void addSignature(byte[] sig) {
+        if (sig == null) {
+            signature = null;
+        } else {
+            signature = Arrays.copyOf(sig, sig.length);
+        }
+    }
+
+    /**
+     * Get raw data of input without signature that can be used for signature
+     * @return List of bytes
+     */
+    public ArrayList<Byte> getRawDataWithoutSignature() {
+
+        ArrayList<Byte> rawData = new ArrayList<>();
+
+        // Add previous hash to signature data
+        if (this.prevTxHash != null) {
+            for (byte b : this.prevTxHash) {
+                rawData.add(b);
+            }
+        }
+        // Add output index to signature data
+        ByteBuffer outputIndexBytes = ByteBuffer.allocate(Integer.SIZE / 8);
+        outputIndexBytes.putInt(this.outputIndex);
+
+        for (byte b : outputIndexBytes.array()) {
+            rawData.add(b);
+        }
+
+        return rawData;
+    }
+
+    /**
+     * Get raw data with signature
+     * @return List of bytes
+     */
+    public ArrayList<Byte> getRawDataWithSignature() {
+
+        ArrayList<Byte> rawData = new ArrayList<>(this.getRawDataWithoutSignature());
+        if (signature != null) {
+
+            for (byte aSignature : this.signature) {
+                rawData.add(aSignature);
+            }
+        }
+
+        return rawData;
+    }
+}
---
+++ src/main/java/coin/Output.java
@@ -0,0 +1,51 @@
+package coin;
+
+import java.nio.ByteBuffer;
+import java.security.PublicKey;
+import java.util.ArrayList;
+
+/**
+ * Transaction output
+ */
+public class Output {
+
+    /** The value of the output */
+    public double value;
+
+    /** The address or public key of the recipient */
+    public PublicKey address;
+
+    /**
+     * Constructor
+     * @param value Value of the output
+     * @param address Public key to which the value is paid
+     */
+    public Output(double value, PublicKey address) {
+
+        this.value = value;
+        this.address = address;
+    }
+
+    /**
+     * Get raw data of output that can be used for signature
+     * @return List of bytes
+     */
+    public ArrayList<Byte> getRawData() {
+
+        ArrayList<Byte> rawData = new ArrayList<>();
+
+        ByteBuffer valueBytes = ByteBuffer.allocate(Double.SIZE / 8);
+        valueBytes.putDouble(this.value);
+
+        for (byte b : valueBytes.array()) {
+            rawData.add(b);
+        }
+
+        byte[] addressBytes = this.address.getEncoded();
+        for (byte b : addressBytes) {
+            rawData.add(b);
+        }
+
+        return rawData;
+    }
+}
