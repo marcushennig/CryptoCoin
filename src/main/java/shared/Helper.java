9d9b750ad7022bcc7c53cad19310f11081f42738
Marcus Hennig <marcus.hennig@d-fine.de>
2018-01-15 21:16:41 +0100

    Cleaned up transaction code

---
+++ src/main/java/shared/Helper.java
@@ -0,0 +1,21 @@
+package shared;
+
+import java.util.ArrayList;
+
+public class Helper {
+
+    /**
+     * Convert list of bytes into array
+     * @param bytes List of bytes
+     * @return Array of bytes
+     */
+    public static byte[] ConvertToByteArray(ArrayList<Byte> bytes) {
+
+        byte[] rawBytes = new byte[bytes.size()];
+        int i = 0;
+        for (Byte b : bytes) {
+            rawBytes[i++] = b;
+        }
+        return rawBytes;
+    }
+}
